fc9f2a7dd65f357d28aff89d44a90956666952ad
sravan.ankireddy <sravan8105@gmail.com>
2023-01-23 12:12:21 -0600

    Code clean up -- ISIT

--- backup_v1_10_30/conv/working_v1/find_inner_rate.m
+++
@@ -1,1 +0,0 @@
-function inner_code = 
--- backup_v1_10_30/v1/nrldpc_enc.m
+++
@@ -1,9 +0,0 @@
-function [output] = nrldpc_enc(data_in, rate, modulation, rv, bgn, nlayers)
-    in = int8(data_in);
-    tbIn = nrCRCEncode(in,  '16');
-    cbsIn = nrCodeBlockSegmentLDPC(tbIn, bgn);
-    enc = nrLDPCEncode(cbsIn, bgn);
-    outlen = ceil(length(data_in)/rate);
-    output = nrRateMatchLDPC(enc,outlen,rv,modulation,nlayers);
-    output = double(output);
-end
